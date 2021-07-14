59359bf72ace1027a701c6da575f878447d76a67
Pedro Rodrigues <pedrorbrito2003@gmail.com>
2021-07-13 21:15:46 -0300

    projeta submarino e comeca tiro

--- src/Submarino.java
+++ src/Submarino.java
@@ -1,4 +1,29 @@
 
-public class Submarino {
+public class Submarino implements PodeAtirar {
+	private int vidas;
+	private int mergulhadoresSalvos;
+	private int levelO2;
+	//private boolean mergulhado=false;
+	private Tiro tiro;
+	
+	public void salvarMergulhador() {
+		
+	}
+	
+	public void diminuiLevelO2() {
+		
+	}
+	
+	public void aumentaLevelO2() {
+	//prende o jogador ate encher o oxigenio	
+	}
+	
+	public void verficaLocal() {
+		
+	}
+	
+	public void desembarca() {
+		
+	}
 	
 }
