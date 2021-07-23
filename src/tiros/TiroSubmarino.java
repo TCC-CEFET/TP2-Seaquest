241d6e2250f2ca7cb64717d75c4da7a011ba35c0
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2021-07-22 23:35:02 -0300

    Tira 1 static

--- src/tiros/TiroSubmarino.java
+++ src/tiros/TiroSubmarino.java
@@ -8,10 +8,9 @@ import personagens.caracteristicas.*;
 public class TiroSubmarino extends Tiro {
 	static private String caminhoImagem="sprites\\tiroSubmarino.png";
 	static private int largura=48, altura=4 ;
-	static private int velocidade=800 ;
 	
 	public TiroSubmarino(float x, float y, Direcao direcao, Background fundo) {
-		super(caminhoImagem, x, y, largura, altura, velocidade, direcao, fundo) ;
+		super(caminhoImagem, x, y, largura, altura, 800, direcao, fundo) ;
 		Gdx.audio.newSound(Gdx.files.internal("sounds\\fireTorpedo.mp3")).play() ;
 	}
 
