d2cb794066c1b14b605290dc3bbca6d4d702c5c9
CarlosMMCosta <72697116+CarlosMMCosta@users.noreply.github.com>
2021-07-22 23:47:33 -0300

    Update TiroSubmarino.java

--- src/tiros/TiroSubmarino.java
+++ src/tiros/TiroSubmarino.java
@@ -6,11 +6,13 @@ import ambiente.*;
 import personagens.caracteristicas.*;
 
 public class TiroSubmarino extends Tiro {
+	//variaveis estaticas que guardam o caminho e as dimensoes da imagem do tiro, que nao mudam 
 	static private String caminhoImagem="sprites\\tiroSubmarino.png";
 	static private int largura=48, altura=4 ;
 	
 	public TiroSubmarino(float x, float y, Direcao direcao, Background fundo) {
 		super(caminhoImagem, x, y, largura, altura, 800, direcao, fundo) ;
+		//toca o som de tiro
 		Gdx.audio.newSound(Gdx.files.internal("sounds\\fireTorpedo.mp3")).play() ;
 	}
 
