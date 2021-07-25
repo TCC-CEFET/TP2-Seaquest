2395b8b72b953909d0b3b45e9b329bbfd3b77ebc
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-07-25 15:50:12 -0300

    Update Seaquest.java

--- src/rodar/Seaquest.java
+++ src/rodar/Seaquest.java
@@ -2,12 +2,15 @@ package rodar;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication ;
 
+// Classe que chama o jogo
 public class Seaquest {
 
 	public static void main (String[] args) {
+		// Dimensoes da tela
 		int larguraTela = 800 ;
 		int alturaTela = 480 ;
 		
+		// Inicia o programa
 		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true") ;
 		new LwjglApplication(new Jogo(), "Seaquest", larguraTela, alturaTela, false) ;
 	}
