8d481a4bd5a2961a141328f7e1463374dd7e2fdc
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-07-25 15:58:43 -0300

    Update Mergulhador.java

--- src/personagens/aliados/Mergulhador.java
+++ src/personagens/aliados/Mergulhador.java
@@ -14,20 +14,24 @@ import ambiente.*;
 import personagens.caracteristicas.*;
 import personagens.inimigos.*;
 
+// Classe do mergulhador a ser resgatado
 public class Mergulhador {
 	private Background fundo ;
 	
-	static private int pontos=50;
-	static private float velocidadePadrao=75;
-	static float tempoEntreFramePadrao=0.15f ;
+	// Variaveis das caracteristicas do mergulhadores
+	static private int pontos=50; // Quantidade de pontos que eles valem no momemtno, atualiza de acordo com o ponto do jogo
+	static private float velocidadePadrao=75; // Velocidade padrao do momento, atualiza de acordo com o ponto do jogo
+	static float tempoEntreFramePadrao=0.15f ; // Tempo entre frames padrao para a animacao de todos os mergulhadores
 	
-	static private int largura=52, altura=56 ;	
+	static private int largura=52, altura=56 ; // Demensao da imagem dos mergulhadores
 	
-	static private TextureRegion[] framesDireita ;
+	// Variaveis de animacao
+	static private TextureRegion[] framesDireita ; // Array de txturas em ordem de animacao
 	private Animation animacaoDireita ;
-	static private TextureRegion[] framesEsquerda ;
+	static private TextureRegion[] framesEsquerda ; // Array de txturas em ordem de animacao
 	private Animation animacaoEsquerda ;
 	
+	// Propriedades do mergulhador
 	private Rectangle retangulo ;
 	private Direcao direcao ;
 	private float velocidade ;
@@ -47,6 +51,7 @@ public class Mergulhador {
 		montaAnimacao() ;
 	}
 	
+	// Funcao que monta as animacoes
 	public void montaAnimacao() {
 		String caminhoImagem="sprites\\mergulhador_spritesheet.png" ;
 		int colunas=3, linhas=2 ;
@@ -73,11 +78,13 @@ public class Mergulhador {
 		animacaoEsquerda = new Animation(tempoEntreFrame, framesEsquerda) ;
 	}
 	
+	// Funcao que atualiza a animacao de acordo com a velocidade do mergulhador
 	public void atualizaAnimacao() {
 		animacaoDireita = new Animation(tempoEntreFrame, framesDireita) ;
 		animacaoEsquerda = new Animation(tempoEntreFrame, framesEsquerda) ;
 	}
 	
+	// Funcao que coloca a animacao em tela
 	public void anima(SpriteBatch batch, float stateTime) {
 		TextureRegion frameAtual ;
 		
@@ -87,11 +94,13 @@ public class Mergulhador {
 		batch.draw(frameAtual, retangulo.x, retangulo.y) ;
 	}
 	
+	// Funcao para movimentar o emrgulhador automaticamente
 	public void movimenta() {
 		if (direcao == Direcao.DIREITA) retangulo.x += velocidade * Gdx.graphics.getDeltaTime() ;
 		else retangulo.x -= velocidade * Gdx.graphics.getDeltaTime() ;
 	}
 	
+	// Funcao que verifica se o mergulhaodr deve ser removido quando sai da tela
 	public boolean paraRemover() {
 		if (retangulo.x < 0-largura && this.direcao == Direcao.ESQUERDA) return true ;
 		if (retangulo.x > fundo.getLargura() && this.direcao == Direcao.DIREITA) return true ;
@@ -111,16 +120,19 @@ public class Mergulhador {
 		return pontos ;
 	}
 	
+	// Funcao para aumentar quantos pontos os mergulhadores valem
 	static public void aumentaPontos() {
 		if (pontos < 1000) pontos += 50 ;
 	}
 	
+	// Funcao para regatar o emrgulhador
 	public void some() {
-		Gdx.audio.newSound(Gdx.files.internal("sounds\\rescueDiver.mp3")).play() ;
+		Gdx.audio.newSound(Gdx.files.internal("sounds\\rescueDiver.mp3")).play() ; // Toca o som de resgate
 	}
 	
+	// Funcao para verificar a posicao do mergulhador e verificar o que deve ser executado
 	public void verificaPosicao(ArrayList<Inimigo> inimigos) {
-		// Verifica os inimigos para aumentar velocidade
+		// Verifica se o inimigo esta encostando para aumentar velocidade
 		Iterator<Inimigo> iterInimigos = inimigos.iterator() ;
 		while (iterInimigos.hasNext()) {
 			Inimigo inimigo = iterInimigos.next() ;
@@ -137,6 +149,7 @@ public class Mergulhador {
 			}
 		}
 		
+		// Volta ao normal caso nenhum inimigo esteja encostando
 		animacaoNormal = true ;
 		tempoEntreFrame = tempoEntreFramePadrao ;
 		velocidade = velocidadePadrao ; // Deixa na velocidade padrao
