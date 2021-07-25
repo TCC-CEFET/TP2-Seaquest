11baec5ac6068464d2d45c67f8ef0001a8bc8a95
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2021-07-25 16:00:15 -0300

    Update Direcao.java

--- src/personagens/caracteristicas/Direcao.java
+++ src/personagens/caracteristicas/Direcao.java
@@ -5,13 +5,16 @@ import java.util.Random;
 import ambiente.*;
 import personagens.aliados.*;
 
+// Enum para a direcao em que os personagens estao indo
 public enum Direcao {
 	ESQUERDA, DIREITA ;
 	
+	// Pega uma direcao aleatoria
 	static public Direcao getDirecaoAleatoria() {
 		return new Random().nextFloat() <= 0.5 ? Direcao.DIREITA : Direcao.ESQUERDA ;
 	}
 	
+	// Pega o X inicial de spawn de acordo com a direcao
 	public int getXInicial(int largura, Background fundo) {
 		if (largura == Mergulhador.getLargura()) return this == Direcao.DIREITA ? 0-largura : fundo.getLargura() ;
 		else return this == Direcao.DIREITA ? 0-largura-200 : fundo.getLargura()+200 ;
